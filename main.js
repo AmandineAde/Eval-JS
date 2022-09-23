0d184397ea37b9a6306cfb56299ddc053da453d2
AmandineAde <adenin.amandine@gmail.com>
2022-09-23 14:50:03 +0200

    ajout entrer

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//btnClick ou Entrer
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        let connexion = document.getElementById("modalConnexion").style.display;
+        let inscription = document.getElementById("modalInscription").style.display;
+        let btnConnexion = document.getElementById('btnConnex')
+        let btnInscription = document.getElementById('btnInscription')
+
+        if (connexion != "" && connexion != "none") {
+            btnConnexion.click();
+        } else if (inscription != "" && inscription != "none") {
+            btnInscription.click();
+        }
+    }
+});
+
 //Verifie la connexion
 function verifConnexion() {
 
@@ -12,23 +29,50 @@ function verifConnexion() {
     if (mdpConnex.value.trim() == "") {
         alert('Veuillez entrer votre mot de passe');
         return false;
-    }
-    //stock dans le local storage la connexion
-    var stockage = localStorage.getItem("ForumDeOuf");
-    compte = JSON.parse(stockage);
-    //Si l'e-mail rentrer est différent de l'email stocker, ça affiche un message
-    if (compte.Email != emailPseudoConnex.value.trim() && compte.Pseudo != emailPseudoConnex.value.trim()) {
-        alert('Veuillez vous inscrire');
-        //Si le mot de passe rentrer est différent du mot de passe stocker, ça affiche un message
-    } else if (compte.MotDePasse != mdpConnex.value.trim()) {
-        alert('Mot de passe incorrect')
+    };
+    let user = retrouveUser(emailPseudoConnex.value.trim());
+
+    if (typeof user == "object") {
+        if (user.MotDePasse == mdpConnex.value.trim()) {
+            sessionStorage.user = JSON.stringify(user);
+            window.location.reload();
+        }
     } else {
-        //sinon c'est que tout est bon et je suis rediriger vers la page forum
-        location.replace('forum.html')
-    }
+        alert('Pseudo ou email incorrect')
+    };
 }
+
+function userConnecter() {
+    return sessionStorage.user != null;
+}
+
+
+
+function retrouveUser(identifiant) {
+    let users = JSON.parse(localStorage.users)
+
+    let resultFind = users.find(user => (user.Email === identifiant || user.Pseudo === identifiant));
+
+    return resultFind;
+}
+
+function creerDeconnexionBtn() {
+    let text = "";
+    let signIn = document.getElementById('signlog-in');
+    let forum = document.getElementById('forumConnex');
+    let para = document.getElementById('paraConnex')
+    forum.classList.toggle('disabled');
+    para.classList.add('d-none')
+
+    text += "<button type='submit' class='btn fs-3 text-white' onclick='deconnecting()'><i class='fa-solid fa-user-minus text-white me-2'></i>Déconnexion</button>"
+    signIn.innerHTML = text;
+}
+
+
+
 //Quand on clique sur deconnection on retourne sur index 
 function deconnecting() {
+    sessionStorage.removeItem("user");
     location.replace('index.html')
 }
 
@@ -81,6 +125,8 @@ function verifInscription() {
         return false;
     };
 
+    var users = JSON.parse(localStorage.users)
+
     var compte = {
         Email: emailInscr.value.trim(),
         Pseudo: pseudoInscr.value.trim(),
@@ -92,40 +138,153 @@ function verifInscription() {
         Ville: villeInscr.value.trim(),
         CodePostal: codePostInscr.value.trim()
     };
-    //Je stocke l'inscritpion dans le local storage
-    localStorage.setItem("ForumDeOuf", JSON.stringify(compte));
-    console.log(compte);
 
+    users.push(compte)
+    localStorage.users = JSON.stringify(users)
 }
 
+let carteCreer = document.getElementById('carteCreer')
 
 if (localStorage.getItem('Objet') != null) {
-    titreObjet.textContent = `${localStorage.getItem('Objet')}`;
+    //  titreObjet.textContent = `${localStorage.getItem('Objet')}`;
 }
 if (localStorage.getItem('Contenu') != null) {
-    titreContenu.textContent = `${localStorage.getItem('Contenu')}`;
+    // titreContenu.textContent = `${localStorage.getItem('Contenu')}`;
 }
 
-envoyerContenu.onclick = () => {
+
+let envoyerContenu = document.getElementById('envoyerContenu')
+
+if (envoyerContenu != undefined) {
+    envoyerContenu.onclick = () => {
+
+        var sujets = JSON.parse(localStorage.sujets)
+
+        let sujetForum = document.getElementById('objetForum')
+        let contenuForum = document.getElementById('contenuForum')
+
+        var sujet = {
+            Id: sujets.length,
+            Sujet: sujetForum.value.trim(),
+            Contenu: contenuForum.value.trim(),
+            Likes: [],
+            Dislikes: []
+        };
+        sujets.push(sujet);
+        localStorage.sujets = JSON.stringify(sujets)
+        window.location.reload();
+    }
+}
+
+
+if (carteCreer != null) {
+    console.log(3 + 5)
+}
+
+function addLike(id) {
+    var sujets = JSON.parse(localStorage.sujets)
+    let like = {
+        auteur: "qql",
+    }
+
+
+    sujets[id].Likes.push(like);
+    localStorage.sujets = JSON.stringify(sujets)
+    window.location.reload();
+}
+
+function addDislike(id) {
+    var sujets = JSON.parse(localStorage.sujets)
+    let dislike = {
+        auteur: "qql",
+    }
+    sujets[id].Dislikes.push(dislike);
+    localStorage.sujets = JSON.stringify(sujets)
+    window.location.reload();
+}
+
+function creerCarte(data) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    let card = document.createElement('div');
+    card.classList.add('card', 'p-0');
+    row.appendChild(card);
+
+    let cardHeader = document.createElement('h4');
+    cardHeader.classList.add('card-header', "fw-bold");
+    cardHeader.textContent = data.Sujet
+    card.appendChild(cardHeader);
+
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    card.appendChild(cardBody);
+
+    let cardText = document.createElement('p');
+    cardText.classList.add('card-text')
+    cardText.setAttribute('id', 'titreContenu')
+    cardText.textContent = data.Contenu
+    cardBody.appendChild(cardText);
+
+    let likeDiv = document.createElement('div');
+    likeDiv.classList.add('float-end')
+    cardBody.appendChild(likeDiv);
+
+    let buttonLike = document.createElement('a');
+    buttonLike.classList.add('me-3', 'text-success', "fw-bold", "fs-5")
+    //buttonLike.setAttribute('data-id', data.Id)
+    buttonLike.onclick = function (event) {
+        addLike(data.Id)
+    }
+    buttonLike.textContent = data.Likes.length
+    likeDiv.appendChild(buttonLike)
+
+    let buttonDislike = document.createElement('a');
+    buttonDislike.classList.add('text-danger', "fw-bold", "fs-5");
+    buttonDislike.textContent = data.Dislikes.length;
+    buttonDislike.onclick = function (event) {
+        addDislike(data.Id)
+    }
+    likeDiv.appendChild(buttonDislike);
+
+
+    let iconeLike = document.createElement('i');
+    iconeLike.classList.add('fa-solid', 'fa-thumbs-up', "ms-1", "fs-5")
+    buttonLike.appendChild(iconeLike)
+
+
+    let iconeDislike = document.createElement('i');
+    iconeDislike.classList.add('fa-solid', 'fa-thumbs-down', "ms-1", "fs-5")
+    buttonDislike.appendChild(iconeDislike)
+
+
+
+
+    carteCreer.appendChild(row);
+}
+
+
+
+window.addEventListener('load', (event) => {
     var sujets = []
+    var users = []
+    if (localStorage.users != null) {
+        users = JSON.parse(localStorage.users);
+    } else {
+        localStorage.users = JSON.stringify(users)
+    };
+
     if (localStorage.sujets != null) {
         sujets = JSON.parse(localStorage.sujets);
     } else {
         localStorage.sujets = JSON.stringify(sujets)
     };
-
-
-    let sujetForum = document.getElementById('objetForum')
-    let contenuForum = document.getElementById('contenuForum')
-
-    var sujet = {
-        Sujet: sujetForum.value.trim(),
-        Contenu: contenuForum.value.trim(),
-
-    };
-    sujets.push(sujet);
-    localStorage.sujets = JSON.stringify(sujets)
-    window.location.reload();
-}
-
-
+    if (envoyerContenu != null) {
+        for (let index = 0; index < sujets.length; index++) {
+            creerCarte(sujets[index])
+        }
+    }
+    if (userConnecter()) {
+        creerDeconnexionBtn()
+    }
+});
